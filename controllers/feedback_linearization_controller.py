0f160fe0bd48513cfef7ece63f44d15f36141b92
Bartosz <bartptak@gmail.com>
2020-05-26 11:42:37 +0200

    Takie cos

--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -9,11 +9,11 @@ class FeedbackLinearizationController(Controller):
         self.kd = np.diag((1,1)) * [1, -3]
         self.kp = np.diag((1,1)) * [15, 5]
 
-    def calculate_control(self, x, v, q_d, q_d_dot):
+    def calculate_control(self, x, q_d, q_d_dot, q_dd_dot):
         """
         Please implement the feedback linearization using self.model (which you have to implement also),
         robot state x and desired control v.
         """
-        v = v + self.kd.dot(x[2:].reshape(-1, 1)-q_d_dot.reshape(-1, 1)) + self.kp.dot(x[:2].reshape(-1, 1)-q_d.reshape(-1, 1))
+        v = q_dd_dot + self.kd.dot(x[2:].reshape(-1, 1)-q_d_dot.reshape(-1, 1)) + self.kp.dot(x[:2].reshape(-1, 1)-q_d.reshape(-1, 1))
 
         return self.model.M(x).dot(v) + self.model.C(x).dot(x[2:].reshape(-1, 1))
