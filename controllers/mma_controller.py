34dbb7b1f5b6880cc7b11442fb7124a535f6b524
Mateusz <piechocki.mateusz97@gmail.com>
2020-05-26 13:07:34 +0200

    Add task2 config

--- controllers/mma_controller.py
+++ controllers/mma_controller.py
@@ -1,20 +1,26 @@
 import numpy as np
 from .controller import Controller
+from models.manipulator_model import ManiuplatorModel
 
 
 class MMAController(Controller):
     def __init__(self, Tp):
         # TODO: Fill the list self.models with 3 models of 2DOF manipulators with different m3 and r3
         # Use parameters from manipulators/mm_planar_2dof.py
-        self.models = [None, None, None]
+        self.m3 = []
+        self.r3 = []
+        self.models = [ManiuplatorModel(Tp), ManiuplatorModel(Tp), ManiuplatorModel(Tp)]
         self.i = 0
+        self.kd = np.diag((1,1)) * [1, -3]
+        self.kp = np.diag((1,1)) * [15, 5]
 
     def choose_model(self, x, u, x_dot):
         # TODO: Implement procedure of choosing the best fitting model from self.models (by setting self.i)
         pass
 
-    def calculate_control(self, x, desired_q_ddot):
-        v = desired_q_ddot  # TODO: Add Feedback
+    def calculate_control(self, x, q_d, q_d_dot, q_dd_dot):
+        #v = desired_q_ddot  # TODO: Add Feedback
+        v = q_dd_dot + self.kd.dot(x[2:].reshape(-1, 1)-q_d_dot.reshape(-1, 1)) + self.kp.dot(x[:2].reshape(-1, 1)-q_d.reshape(-1, 1))
         q_dot = x[2:, np.newaxis]
         M = self.models[self.i].M(x)
         return M @ (v + np.linalg.inv(M) @ self.models[self.i].C(x) @ q_dot)
